dde40cd4e11d92037a9f243e054fdeb8e2687969
= <=>
2021-07-01 02:43:40 -0600

    Add: Base file

---
+++ app/src/main/java/com/greentm/heventapps/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.greentm.heventapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
