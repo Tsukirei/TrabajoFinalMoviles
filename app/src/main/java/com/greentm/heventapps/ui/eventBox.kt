69063de4708ac41a602e64c4b40bb821ca5e9adf
= <=>
2021-07-02 06:56:34 -0600

    Add: Evente fragment, event format fragment, comentary fragment

---
+++ app/src/main/java/com/greentm/heventapps/ui/eventBox.kt
@@ -0,0 +1,30 @@
+package com.greentm.heventapps.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.greentm.heventapps.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+class eventBox : Fragment() {
+    // TODO: Rename and change types of parameters
+
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_eventBox, container, false)
+    }
+
+
+}
